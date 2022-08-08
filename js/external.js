5c46082c009c8dd7574a1691c9c34b428309d032
Cristian Almendarez <cristian.t.almendarez@gmail.com>
2022-08-08 16:38:54 -0500

    Math exercises

--- js/external.js
+++ js/external.js
@@ -1,15 +1,33 @@
 "use strict";
 
+console.log("Hello from external Javascript");
+alert("Welcome to my Website!");
 
-var myAge = 25;
-console.log(myAge);
-alert("this is my external file");
-//This reads last because js reads top to bottom
-alert("this is my second alert");
+var favoriteColor = prompt("What is your favorite color?")
+alert("Great, " + favoriteColor + " is my favorite color too")
 
-var isStudent = confirm(" Click okay if you are you a student");
-console.log(isStudent);
+var littleMerDays = prompt("How many days did you rent the Little Mermaid?")
+var broBearDays = prompt("How many days did you rent Brother bear?")
+var herDays = prompt("How many days did you rent Hercules?")
+const cost = 3
+var total = (littleMerDays * cost) + (broBearDays * cost) + (herDays * cost)
+alert("The total cost of these three movies, " + total)
 
-var firstName = prompt("What is your first name?")
-var lastName = prompt("What is your last name?")
-console.log(firstName + lastName);
+var google = prompt("How many hours did you work at google?")
+var amazon = prompt("How many hours did you work at amazon?")
+var facebook = prompt("How many hours did you work at facebook?")
+var total = (google * 400) + (amazon * 380) + (facebook * 350)
+alert("Your total pay for the week is, " + total)
+
+var enrollIfFreeAndAvail = confirm('Are you free and wish to enroll?')
+if(enrollIfFreeAndAvail)
+    alert("Click here to enroll")
+
+
+
+
+
+
+
+
+
